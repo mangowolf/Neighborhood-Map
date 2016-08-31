31a123b786bb293ebf66055c1d1ac89b23571c51
mangowolf <jimmyz949@hotmail.com>
2016-08-30 23:28:11 -0700

    Info window populating with Yelp API call info!

--- js/app.js
+++ js/app.js
@@ -1,49 +1,15 @@
-/**
- * Generates a random number and returns it as a string for OAuthentication
- * @return {string}
- */
-function nonce_generate() {
-  return (Math.floor(Math.random() * 1e12).toString());
-}
-
-var yelp_url = "http://api.yelp.com/v2/search";
-
-var parameters = {
-  oauth_consumer_key: 'QJR3xjIwW9d9jnCjLBTzXQ',
-  oauth_token: 'zEwwDoGQU78dheH4id-wxVpaR5jwyDu2',
-  oauth_nonce: nonce_generate(),
-  oauth_timestamp: Math.floor(Date.now()/1000),
-  oauth_signature_method: 'HMAC-SHA1',
-  oauth_version : '1.0',
-  callback: 'cb',          // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-  location: 'New+York',
-  term: 'food',
-  limit: 1
-};
-
-var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, 'tc2X8lqfCR4GeVCoStbnPyo4nds', 'vXE_E2P7ryml6qhzqYaObKLhcdA');
-parameters.oauth_signature = encodedSignature;
-
-var settings = {
-  url: yelp_url,
-  data: parameters,
-  cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-  dataType: 'jsonp',
-  success: function(results) {
-    console.log(results);
-  },
-  error: function() {
-    console.log('failed');
-  }
-};
-
-// Send AJAX query via jQuery library.
-$.ajax(settings);
-
 // Normally we'd have these in a database instead.
 var model = {
 	currentLocation: null,
 	locations: [
+	{
+		title: 'Los Tacos No.1',
+		yelpURL: 'http://api.yelp.com/v2/business/los-tacos-no-1-new-york',
+		location: {
+			lat: 40.742455,
+			lng: -74.005956
+		},
+	},
 	{
 		title: 'Park Ave Penthouse',
 		location: {
@@ -116,6 +82,67 @@ ViewModel.filteredLocations = ko.computed(function(){
 	}
 },ViewModel);
 
+var yelpAPI = function(){
+	/**
+	 * Generates a random number and returns it as a string for OAuthentication
+	 * @return {string}
+	 */
+	function nonce_generate() {
+	  return (Math.floor(Math.random() * 1e12).toString());
+	}
+
+	//var yelpBaseURL = "http://api.yelp.com/v2/business";
+	//var yelp_url = yelpBaseURL + model[i].yelpURL;
+	var yelp_url = model.locations[0].yelpURL;
+
+	var parameters = {
+	  oauth_consumer_key: 'QJR3xjIwW9d9jnCjLBTzXQ',
+	  oauth_token: 'zEwwDoGQU78dheH4id-wxVpaR5jwyDu2',
+	  oauth_nonce: nonce_generate(),
+	  oauth_timestamp: Math.floor(Date.now()/1000),
+	  oauth_signature_method: 'HMAC-SHA1',
+	  oauth_version : '1.0',
+	  callback: 'cb',          // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+	  limit: 1
+	};
+
+	var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, 'tc2X8lqfCR4GeVCoStbnPyo4nds', 'vXE_E2P7ryml6qhzqYaObKLhcdA');
+	parameters.oauth_signature = encodedSignature;
+
+	var settings = {
+	  url: yelp_url,
+	  data: parameters,
+	  cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+	  dataType: 'jsonp',
+	  success: function(results) {
+	    console.log(results);
+	  },
+	  error: function() {
+	    console.log('failed');
+	  }
+	};
+
+	// Send AJAX query via jQuery library.
+	$.ajax(settings).done(function(results){
+		var yelpLocations = results.reviews;
+		model.locations[0].result = results;
+		model.locations[0].rating = results.rating;
+		console.log(model.locations[0].rating);
+		/*yelpLocations.forEach(function(review){
+			var loc = {};
+			loc.rating = location.rating;
+			loc.excerpt = location.excerpt;
+			//loc.lat = location.location.coordinate.latitude;
+			//loc.lng = location.location.coordinate.longitude;
+			model.locations.push(loc);
+			console.log(loc);
+			model.locations[0].rating = review.rating;
+			model.locations[0].excerpt = review.excerpt;
+			console.log(review);
+		});*/
+	});
+};
+
 //Pops up information window over marker when corresponding list item is selected
 ViewModel.curLocation = function(curLoc){
 	google.maps.event.trigger(curLoc.markerLocation,'click');
@@ -161,9 +188,14 @@ function initMap() {
 
 		bounds.extend(ViewModel.locationList()[i].markerLocation.position);
 	}
+
+	yelpAPI();
+
     // Extend the boundaries of the map for each marker
 	map.fitBounds(bounds);
 };
+
+
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
@@ -172,7 +204,8 @@ function populateInfoWindow(marker, infowindow){
     // Check to make sure the infowindow is not already opened on this marker.
 	if(infowindow.marker != marker){
 		infowindow.marker = marker;
-		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.setContent('<div>' + marker.title + '</div>' + '<h3>' + model.locations[0].rating + '</h3>');
+		//infowindow.setContent('<h3>' + model.locations[0].rating + '</h3>');
 		infowindow.open(map,marker);
         // Make sure the marker property is cleared if the infowindow is closed.
 		infowindow.addListener('closeClick', function(){
@@ -187,3 +220,4 @@ var mappedData = ko.utils.arrayMap(model.locations, function(item){
 
 ViewModel.locationList(mappedData);
 ko.applyBindings(ViewModel);
+
